182a13a69886f6210a5064a4213fcdf4cec715a7
prabhatm102 <prabhat@rudrainnovative.in>
2022-05-05 17:06:29 +0530

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.40:5000/api/v1/',
-  avatarUrl: 'http://192.168.1.40:5000/avatar/',
-  socketUrl: 'http://192.168.1.40:5000/',
+  apiUrl: 'https://rudra-chat.herokuapp.com/api/v1/',
+  avatarUrl: 'https://rudra-chat.herokuapp.com/avatar/',
+  socketUrl: 'https://rudra-chat.herokuapp.com/',
+  groupIconUrl: 'https://rudra-chat.herokuapp.com/groupIcon/',
 };
